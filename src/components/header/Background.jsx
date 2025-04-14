7915de797ac0a5bf1ac578a5258594dccbe8c0aa
Evgeniy <xaxaevgesha@mail.ru>
2025-04-14 10:10:07 +0700

    second comment

---
+++ src/components/header/Background.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const Background = () => {
+    return (
+        <div>
+            <div className="background--fone"></div>
+        </div>
+    );
+};
+
+export default Background;
---
+++ src/components/header/Search.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            Я тебя в сосиску заверну!
+        </div>
+    );
+};
+
+export default Search;

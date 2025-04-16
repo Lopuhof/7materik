733af935f58f9a4ed377471d6e654d813e6e6903
Evgeniy <xaxaevgesha@mail.ru>
2025-04-16 12:11:58 +0700

    added new files in git

---
+++ src/components/header/background/Background.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './css/style.css';
+
+const Background = () => {
+    return (
+        <div>
+            <div className="background__fone"></div>
+        </div>
+    );
+};
+
+export default Background;

28ff71e26701278d89ce2b3b72e22219d2018b5c
Evgeniy <xaxaevgesha@mail.ru>
2025-05-24 12:42:52 +0700

    category exemplar are complete + margin center of category

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "./components/header/background/Header";
 import Main from "./components/main/Main";
-import Category from "./components/category/Category";
-import Articles from "./components/articles/Articles";
 
 function App() {
     return (
         <div className="App">
-          <Header/>
-          <Main/>
+          <div className="App-content">
+            <Header/>
+          </div>
+          <div className="App-category-fone"></div>
+          <div className="App-content">
+            <Main/>
+          </div>
         </div>
     );
 }

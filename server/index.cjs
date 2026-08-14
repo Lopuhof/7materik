10337e6cc1dac9ac812d91f5ae03278de7826d1a
Evgeniy <xaxaevgesha@mail.ru>
2026-08-14 11:11:24 +0700

    added in server: controllers, dataBases, public, routes, views and .env

--- server/index.cjs
+++ server/index.cjs
@@ -1,12 +1,56 @@
-const Sequelize = require('sequelize');
+/* const Sequelize = require('sequelize');
 const dataBase = require('./dataBases/index.cjs');
 const Users = dataBase.users;
 
 function t01() {
     Users.findAll().then(result => {
-        /* console.log(result); */
         result.forEach(item => console.log(item.password));
     });
 };
 
-t01();
+t01(); */
+
+const express = require('express');
+const path =require('path');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './.env'
+});
+
+const app = express();
+
+const PORT = 5555;
+
+const dataBase = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: '7materik',
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+dataBase.connect( (err) => {
+    if (err === true) {
+        console.log(err);
+    } else {
+        console.log('MYSQL connected...');
+    };
+});
+
+app.use('/', require('./routes/pages.cjs'));
+app.use('/auth', require('./routes/auth.cjs'));
+
+app.listen(PORT, () => {
+    console.log('server started at ' + PORT + ' port')
+});
---
+++ server/routes/auth.cjs
@@ -0,0 +1,8 @@
+const express = require('express');
+const authController = require('../controllers/auth.cjs');
+
+const router = express.Router();
+
+router.post('/register', authController.register)
+
+module.exports = router;
